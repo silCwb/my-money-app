7444448d9350cd34aa3a03ecd88815faa2a0584d
Silvana Siqueira <silcwb@Silvanas-MacBook-Pro.local>
2020-05-08 15:34:10 -0300

    .

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
