407a6bcc565c1c78cfcaf67450143e4548bddb65
Silvana Siqueira <silcwb@Silvanas-MacBook-Pro.local>
2020-05-12 20:56:47 -0300

    .

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -4,13 +4,14 @@ const mongoose = restful.mongoose
 const creditSchema = new mongoose.Schema({
     name: { type: String, required: true },
     value: { type: Number, min: 0, required: true }
-})
+});
 
-const debtSchema = new mongoose.Schema({
+const debitSchema = new mongoose.Schema({
     name: { type: String, required: true },
     value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
     status: { type: String, required: false, uppercase: true,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
+        //enum: ['Mr.', 'Mrs.', 'Ms.']}
 })
 
 const billingCycleSchema = new mongoose.Schema({
@@ -18,7 +19,7 @@ const billingCycleSchema = new mongoose.Schema({
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
     credits: [creditSchema],
-    debts: [debtSchema]
+    debits: [debitSchema]
 })
 
 module.exports = restful.model('BillingCycle', billingCycleSchema)
