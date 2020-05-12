2dce89e55ed0e0064201e429199c5ed0b7036217
Silvana Siqueira <silcwb@Silvanas-MacBook-Pro.local>
2020-05-12 20:57:14 -0300

    .

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -4,14 +4,13 @@ const mongoose = restful.mongoose
 const creditSchema = new mongoose.Schema({
     name: { type: String, required: true },
     value: { type: Number, min: 0, required: true }
-});
+})
 
 const debitSchema = new mongoose.Schema({
     name: { type: String, required: true },
     value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
     status: { type: String, required: false, uppercase: true,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
-        //enum: ['Mr.', 'Mrs.', 'Ms.']}
 })
 
 const billingCycleSchema = new mongoose.Schema({
