7024fed89f79323c65f8e1f8969e44f6d7438fdf
Silvana Siqueira <silcwb@Silvanas-MacBook-Pro.local>
2020-05-12 15:03:12 -0300

    added route to count the registers

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -6,4 +6,14 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
 // permite validação de campos required também na alteração
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]}) 
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 module.exports = BillingCycle 
