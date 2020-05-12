449750cda1bd80fb48033dd4c17fd57834c2cbaa
Silvana Siqueira <silcwb@Silvanas-MacBook-Pro.local>
2020-05-12 14:35:21 -0300

    .

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,4 +2,10 @@ const mongoose = require('mongoose')
 mongoose.promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
 
-mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min =  
+    "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max =  
+    "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = 
+    "'{VALUE}' não é válido para o atributo '{PATH}'."
