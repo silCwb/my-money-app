802df8da849b9b3df2f401dffad5fefebeae5136
Silvana Siqueira <silcwb@Silvanas-MacBook-Pro.local>
2020-05-20 16:32:32 -0300

    error handler created

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -1,10 +1,12 @@
 //definição dos serviços REST: criação dos serviços web (express)
 // integração do mongo com o express
 const BillingCycle = require('./billingCycle') 
+const errorHandler = require('../common/errorHandler')
 
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 // permite validação de campos required também na alteração
 BillingCycle.updateOptions({new: true, runValidators: true})
+BillingCycle.after('post', errorHandler).after('put', errorHandler)
 
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
